3def25395009024ff903f33fd85bd864425cdde3
luizgg <luiz.giordani@outlook.com>
2022-07-05 23:59:53 -0300

    checkpoint developments for images with color

--- funcs/ml.py
+++ funcs/ml.py
@@ -4,29 +4,132 @@ import imageio
 from skimage.util import view_as_blocks
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import IsolationForest
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import silhouette_score
 
-def read_images(images_path):
+## Reading data
+def read_images(images_path, color=False):
     
     n = len(images_path)
-    imgs_array = np.zeros((n, 1024, 1024))
+    if color:
+        imgs_array = np.zeros((n, 1024, 1024, 3))
 
-    for i in range(n):
+        for i in range(n):
 
-        img = imageio.imread(images_path[i])
-        imgs_array[i, :, :] = img
+            img = imageio.imread(images_path[i])
+            imgs_array[i, :, :, :] = img
+    else:    
+        imgs_array = np.zeros((n, 1024, 1024))
+
+        for i in range(n):
+
+            img = imageio.imread(images_path[i])
+            imgs_array[i, :, :] = img
 
     return imgs_array
 
 
 def images_as_patches(imgs_array, patch_size):
 
-    # TODO: Check for the case when there are colors (3 channesl)
     width = patch_size[1]
+    channels = imgs_array.shape[-1] < 4
      # We flatten the patches since we will take the histogram and the matrix format isn't necessary
-    img_patches = view_as_blocks(imgs_array, patch_size).copy().reshape(-1, width**2)
+   
+    if channels:
+        img_patches = view_as_blocks(imgs_array, patch_size).copy().reshape(-1, width**2, 3)
+    else: 
+        img_patches = view_as_blocks(imgs_array, patch_size).copy().reshape(-1, width**2)    
 
     return img_patches
 
+## Outlier detection
+class IntensityHistogram(TransformerMixin, BaseEstimator):
+    """ 
+    Sklearn compatible method to compute the intensity histogram. It returns the normalized histogram.
+    (i.e the frequencies divided by their sum).
+
+    Parameters
+    ----------
+    n_bins : int, default=255
+        The number of bins on the histogram.
+    """
+
+    def __init__(self, nbins=255):
+        self.nbins = nbins
+
+    def fit(self, X, y=None):
+
+        return self.fit_transform(X)
+
+    def transform(self, X):
+       
+        return self.fit_transform(X)
+    
+    def fit_transform(self, X, y=None):
+
+        matrix_shape = X.shape
+
+        if len(matrix_shape) < 3:
+            return self.histogram_gray(X)
+        else:
+            return self.histogram_color(X)
+
+    def histogram_gray(self, X):
+
+        n = X.shape[0]
+        new_X = np.zeros((n, self.nbins))
+
+        for i in range(n):
+            bins, _ = np.histogram(X[i, :], bins=self.nbins, range=(0, 255))
+            bins = bins / bins.sum()
+
+            new_X[i, :] = bins
+
+        return new_X
+
+    def histogram_color(self, X):
+
+        n = X.shape[0]
+        new_X = np.zeros((n, self.nbins*3))
+
+        for i in range(n):
+            red_bins, _ = np.histogram(X[i, :, 0], bins=self.nbins, range=(0, 255))
+            green_bins, _ = np.histogram(X[i, :, 1], bins=self.nbins, range=(0, 255))
+            blue_bins, _ = np.histogram(X[i, :, 2], bins=self.nbins, range=(0, 255))
+            
+            red_bins = red_bins / red_bins.sum()
+            green_bins = green_bins / green_bins.sum()
+            blue_bins = blue_bins / blue_bins.sum()
+
+            new_X[i, :] = np.concatenate((red_bins, green_bins, blue_bins))
+
+        return new_X
+
+def train_isoforest(X, nbins, ntrees=100, seed=1):
+
+    pipeline = Pipeline([('feature_descriptor', IntensityHistogram(nbins=nbins)), # Method to compute the intensity Histogram
+                    ('clf', IsolationForest(n_estimators=ntrees, n_jobs=-1, random_state=seed))])
+
+    pipeline.fit(X)
+
+    return pipeline
+
+def eval_model(X, pipeline):
+
+    # We are getting the outliers as defined by the metric. Which is not ideal, but it is useful
+    predicted_outliers = pipeline.predict(X)
+
+    transformed_x = pipeline.steps[0][1].fit_transform(X) # Accessing the histogram method
+
+    # Getting the silhouette score which is a metric to evaluate the homogeneity of clusters
+    # While it is not measured on our ideal clusters, i.e the objects, a good anomaly detection system will split into groups that are very dissimilar
+    score = silhouette_score(transformed_x, predicted_outliers)
+    
+    return score
+
+
+## Plotting results
 def prediction_intensities(model, image, patch_size):
     """
     Takes the scores from the model and creates an array of the same shape as the images to use as an overlay of intensities.
@@ -36,7 +139,7 @@ def prediction_intensities(model, image, patch_size):
     n = img_patches.shape[0]
     width = patch_size[0]
 
-    scores = model.score_samples(img_patches)*-1 # To make the interpretatione easier, I want the value of the abnormal observations to be higher
+    scores = model.score_samples(img_patches)*-1 # To make the interpretation easier, I want the value of the abnormal observations to be higher
 
     scores = MinMaxScaler().fit_transform(scores.reshape(-1, 1))
 
@@ -63,44 +166,7 @@ def reconstruct_image(image_patches, image, patch_size):
     reconstructed_image = np.vstack(stacks)
 
     return reconstructed_image
-    
 
-class IntensityHistogram(TransformerMixin, BaseEstimator):
-    """ 
-    Sklearn compatible method to compute the intensity histogram. It returns the normalized histogram.
-    (i.e the frequencies divided by their sum).
-
-    Parameters
-    ----------
-    n_bins : int, default=255
-        The number of bins on the histogram.
-    """
-
-    def __init__(self, nbins=255):
-        self.nbins = nbins
-
-    def fit(self, X, y=None):
-
-        return self.fit_transform(X)
-
-    def transform(self, X):
-       
-        return self.fit_transform(X)
-    
-    def fit_transform(self, X, y=None):
-
-        # TODO: Implement for 3 channels
-
-        n = X.shape[0]
-        new_X = np.zeros((n, self.nbins))
-
-        for i in range(n): # This might not scale with more images/channels/patches
-            bins, _ = np.histogram(X[i, :], bins=self.nbins, range=(0, 255))
-            bins = bins / bins.sum()
-
-            new_X[i, :] = bins
-
-        return new_X
 
 def pairwise(iterable):
     # pairwise('ABCDEFG') --> AB BC CD DE EF FG
